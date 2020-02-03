bb33bc1eb3bc003cfa9e90ee992fb2ed78a7a838
Kevin <kevin12kim27@gmail.com>
2020-02-02 22:10:52 -0800

    minor edits

--- algs/Mowing.java
+++ algs/Mowing.java
@@ -1,5 +1,7 @@
+
 /*
  * Kevin Kim bruh
+ * This uses floodfill algorithm.
  */
 
 import java.io.BufferedReader;
@@ -11,13 +13,11 @@ import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
 public class Mowing {
-	public static void main(String[] args) throws Exception
-	{
+	public static void main(String[] args) throws Exception {
 		long startTime = System.nanoTime();
 
 		// code
 
- 
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("mowing.out")));
 		BufferedReader br = new BufferedReader(new FileReader("mowing.in"));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -25,118 +25,100 @@ public class Mowing {
 		int w = Integer.parseInt(st.nextToken());
 		int startrow = Integer.parseInt(st.nextToken());
 		int startcol = Integer.parseInt(st.nextToken());
-		
+
 		/*
 		 * initialize lawn
 		 */
 		String[][] oglawn = new String[l][w];
 		String[][] biglawn = new String[l][w];
 		boolean lawn[][] = new boolean[l][w];
-		for (int i = 0; i < l; i++)
-		{
-			
+		for (int i = 0; i < l; i++) {
+
 			String[] s = br.readLine().split(" ");
 			biglawn[i] = s;
 			oglawn[i] = s.clone();
-			for (int j = 0; j < w; j++)
-			{
-				if (s[j].equals(".")) lawn[i][j] = true;
+			for (int j = 0; j < w; j++) {
+				if (s[j].equals("."))
+					lawn[i][j] = true;
 			}
 		}
-		
-		
+
 		/*
-		 * set up boolean matrix of same size, with patch center indices reporting 1 or 0 if a mower can go on them.
+		 * set up boolean matrix of same size, with patch center indices reporting 1 or
+		 * 0 if a mower can go on them.
 		 */
 		int[][] adj = new int[l][w];
-		//int counter = 0;
-		for (int j = 1; j < l-1; j++)
-		{
-			for (int i = 1; i < w-1; i++)
-			{
-				if (patchclear(j,i, lawn))
-				{
+		// int counter = 0;
+		for (int j = 1; j < l - 1; j++) {
+			for (int i = 1; i < w - 1; i++) {
+				if (patchclear(j, i, lawn)) {
 					adj[j][i] = 1;
 				}
 			}
 		}
-		
-		
-		
 
 		fill(startrow, startcol, adj, biglawn);
 		printmat(oglawn);
 		System.out.println("********************************************************");
 		printmat(biglawn);
 		long endTime = System.nanoTime();
-		System.out.println("Took "+(endTime - startTime)/(1e6) + " ms");
-		
-		
+		System.out.println("Took " + (endTime - startTime) / (1e6) + " ms");
+
 	}
+
 	/*
 	 * floodfill
 	 */
-	public static void fill(int i, int j, int[][] avail, String[][] lawn)
-	{
-		if (i == 0 || j == 0 || i == lawn.length || j == lawn[0].length || avail[i][j] == 2 || avail[i][j] == 0)
-		{
+	public static void fill(int i, int j, int[][] avail, String[][] lawn) {
+		if (i == 0 || j == 0 || i == lawn.length || j == lawn[0].length || avail[i][j] == 2 || avail[i][j] == 0) {
 			return;
-		}
-		else
-		{
+		} else {
 			avail[i][j] = 2;
-			lawn[i-1][j-1] = "c";
-			lawn[i-1][j] = "c";
-			lawn[i-1][j+1] = "c";
-			lawn[i][j-1] = "c";
+			lawn[i - 1][j - 1] = "c";
+			lawn[i - 1][j] = "c";
+			lawn[i - 1][j + 1] = "c";
+			lawn[i][j - 1] = "c";
 			lawn[i][j] = "c";
-			lawn[i][j+1] = "c";
-			lawn[i+1][j-1] = "c";
-			lawn[i+1][j] = "c";
-			lawn[i+1][j+1] = "c";
-			
-			fill(i+1, j ,avail, lawn);
-			fill(i-1, j , avail, lawn);
-			fill(i,j+1,avail, lawn);
-			fill(i,j-1,avail,lawn);
+			lawn[i][j + 1] = "c";
+			lawn[i + 1][j - 1] = "c";
+			lawn[i + 1][j] = "c";
+			lawn[i + 1][j + 1] = "c";
+
+			fill(i + 1, j, avail, lawn);
+			fill(i - 1, j, avail, lawn);
+			fill(i, j + 1, avail, lawn);
+			fill(i, j - 1, avail, lawn);
 		}
 	}
+
 	/*
 	 * returns whether a square 9 area patch with center r, c is free of obstacles.
 	 */
-	public static boolean patchclear(int r, int c, boolean[][] lawn)
-	{
-		for (int i = r-1; i <= r+1; i++)
-		{
-			for (int j = c-1; j <= c+1; j++)
-			{
-			
-				if (lawn[i][j] == false)
-				{
-					
+	public static boolean patchclear(int r, int c, boolean[][] lawn) {
+		for (int i = r - 1; i <= r + 1; i++) {
+			for (int j = c - 1; j <= c + 1; j++) {
+
+				if (lawn[i][j] == false) {
+
 					return false;
 				}
 			}
 		}
 		return true;
 	}
-	public static void printmat(String[][] matrix)
-	{
-		for (int i = 0 ; i < matrix.length; i++)
-		{
-			for (int j = 0; j < matrix[0].length ; j++)
-			{
+
+	public static void printmat(String[][] matrix) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
 				System.out.print(matrix[i][j] + " ");
 			}
 			System.out.println();
 		}
 	}
-	public static void printmat2(int[][] matrix)
-	{
-		for (int i = 0 ; i < matrix.length; i++)
-		{
-			for (int j = 0; j < matrix[0].length ; j++)
-			{
+
+	public static void printmat2(int[][] matrix) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
 				System.out.print(matrix[i][j] + " ");
 			}
 			System.out.println();
